66d6e275bf249c79fa25122179817ed21aa36867
hamzaboutaleb <hamza-boutaleb@live.fr>
2024-02-19 16:41:56 +0100

    new branch

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import User from './models/user.js';
 
 dotenv.config();
 
@@ -16,21 +17,20 @@ const PORT = process.env.PORT || 5000;
 
 const CONNECTION_URL = process.env.MONGODB_URI;
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+    await User.create({
+        username: "test",
+        email: "hello",
+        password: "test"
+    })
     res.send('Hello!');
 });
 
-app.use('/user', userRoutes);
-
 mongoose.connect(
-    CONNECTION_URL,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }
+    CONNECTION_URL
 )
-    .then (() => app.listen(PORT, () => {
+    .then(() => app.listen(PORT, () => {
         console.log(`Server running on port: ${PORT}`)
     }))
-    .catch ((error) => console.log(error.message));
+    .catch((error) => console.log(error.message));
 
